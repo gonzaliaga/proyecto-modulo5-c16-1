5e18c14262054e0d75935ab8ad8a39aace445cb7
Gonzalo Cuellar Aliaga <111820928+gonzaliaga@users.noreply.github.com>
2025-01-03 20:56:39 -0300

    Sticky Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Router } from './Router'
 
-
+//hicimos que la barra sea sticky!!! uhu!
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
